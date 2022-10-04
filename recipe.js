dbefd415e902f2dea637b5634343de89d18de963
Daniel <dan.earp@icloud.com>
2022-10-04 12:33:11 +0100

    Initial upload

---
+++ recipe.js
@@ -0,0 +1,23 @@
+//These are the ingredients needed for the recipe
+let spaghetti = "400g"
+let guanciale = "250g"
+let yolks = 6
+let cheese = 50
+pepper = "4g"
+
+//This is the weight of an egg to work out the weight of the egg & cheese mixture
+let eggWeight = 50
+
+//This is the step by step guide to making Spaghetti Carbonara
+console.log ("--- How to make Spaghetti Carbonara ---")
+console.log ("1) Start by cutting the ",  spaghetti,"of spagehtti into 1cm layers then into long strips")
+let eggCheeseMixture = eggWeight * yolks + cheese
+console.log ("2) Add the", yolks, "yolks to the", cheese, "of cheese to create a", eggCheeseMixture, "g mixture")
+console.log ("3) Roast the black pepper and add a small amount to the yolk & cheese mixture")
+console.log ("4) Brown the", guanciale, "of guanciale for 3 minutes on a medium heat, then 1 minute on a high heat. Remove from the pan, leaving only the grease and allow pan to cool")
+console.log ("5) Cook the spagehtti with a pinch of salt until al dente, then set aside a ladleful of the pasta water before draining")
+console.log ("6) Add 2 spoons of the hot water into pan with cooled guanciale and turn on heat")
+console.log("7) Turn off the heat and set aside for 1 minute before adding pasta to the pan and mixing together")
+console.log("--- Warning: This part should be done quickly to prevent the eggs becoming scrambled --- ");
+console.log ("8) Add the", eggCheeseMixture, "g egg & cheese mixture to the warm pan, stirring rapidly. The eggs will slowly cook and become creamy.")
+console.log ("9) Add the remaining black pepper and serve immediately")
